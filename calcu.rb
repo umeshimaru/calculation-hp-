b305eec7587a8db12b56c00f76703dfadc161ea6
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-09-01 07:05:32 +0000

    更新

--- calcu.rb
+++ calcu.rb
@@ -1,1 +1,19 @@
-<p>まさ</p>
+hp = 100
+puts "敵HPは100です"
+puts "技を選んでください『かえんほうしゃ,ハイドロポンプ,ソーラービーム』"
+while hp == 0 
+inputs = gets.chomp
+case  inputs
+when 'かえんほうしゃ'
+   hp -= 25 
+   puts "残hp#{hp}"
+when "ﾊｲﾄﾞﾛﾎﾟﾝﾌﾟ"
+   hp -= 20
+   puts "残hp#{hp}"
+when 'ｿｰﾗｰﾋﾞｰﾑ'
+   hp -= 50
+   puts "残hp#{hp}"
+ else 
+   puts '技名が間違っています'
+end
+end 
